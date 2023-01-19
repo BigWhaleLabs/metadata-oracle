07d5e0b91ac6b93f7b03cadb3c4d420298e64c33
sarmat <skhachirov12@gmail.com>
2023-01-19 18:31:21 -0300

    remove unused deps

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import 'module-alias/register'
 import 'source-map-support/register'
 
-import runApp from '@/helpers/runApp'
 import startContractListeners from '@/helpers/startContractListeners'
 
-void (async () => {
-  await runApp()
+void (() => {
+  console.log('Listener started...')
   startContractListeners()
 })()
