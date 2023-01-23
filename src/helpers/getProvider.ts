78da254cec8b11837ec2cf1e8737d64da2644166
sarmat <skhachirov12@gmail.com>
2023-01-23 16:26:10 -0300

    add alchemy evm chains support

--- src/helpers/getProvider.ts
+++ src/helpers/getProvider.ts
@@ -6,6 +6,18 @@ export default function getProvider(chainId: number) {
       return providers.mainnetProvider
     case 5:
       return providers.goerliProvider
+    case 137:
+      return providers.polygonProvider
+    case 80001:
+      return providers.polygonTestnetProvider
+    case 42161:
+      return providers.arbitrumProvider
+    case 421613:
+      return providers.arbitrumTestnetProvider
+    case 10:
+      return providers.optimismProvider
+    case 420:
+      return providers.optimismTestnetProvider
     default:
       return providers.mainnetProvider
   }
