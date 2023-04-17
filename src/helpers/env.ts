fe29f598d05c42931f9a0fc433d5ff9a7cd88c1a
T_DaMER <dpancov64@gmail.com>
2023-04-17 17:17:03 +0300

    Add bwl eslint and prettier configs

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -12,15 +12,15 @@ dotenv.config({ path: resolve(cwd(), '.env') })
 
 // eslint-disable-next-line node/no-process-env
 export default cleanEnv(process.env, {
-  METADATA_CONTRACT_ADDRESS: str({ default: METADATA_LEDGER_CONTRACT_ADDRESS }),
+  ARBITRUM_RPC: str(),
+  ARBITRUM_TESTNET_RPC: str(),
   ETH_NETWORK: str({ default: ETH_NETWORK }),
   ETH_RPC: str({ default: ETH_RPC_MAINNET }),
   GOERLI_RPC: str(),
-  POLYGON_RPC: str(),
-  POLYGON_TESTNET_RPC: str(),
-  ARBITRUM_RPC: str(),
-  ARBITRUM_TESTNET_RPC: str(),
+  METADATA_CONTRACT_ADDRESS: str({ default: METADATA_LEDGER_CONTRACT_ADDRESS }),
   OPTIMISM_RPC: str(),
   OPTIMISM_TESTNET_RPC: str(),
+  POLYGON_RPC: str(),
+  POLYGON_TESTNET_RPC: str(),
   SIGNER_PRIVATE_KEY: str(),
 })
